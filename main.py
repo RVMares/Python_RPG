2b539d894bc9aae0ea0fb5997cee23975f8b41fe
Ruth Hermosillo <hermosilloruth59@gmail.com>
2022-08-02 16:09:49 -0600

    Created Health, Attack Power and Attack Dictionaries for the Characters

---
+++ main.py
@@ -0,0 +1,29 @@
+legendary_monsters = ['Nemean Lion', 'Lernaean Hydra', 'Cerberus']
+hercules_hit_points = 188
+hercules_attack_power = 38
+hercules_attack_dict = {
+    'Unarmed Attack': -11,
+    'Spear Melee Attack': -12,
+    'Spear Ranged Attack': -12,
+    'Longbow Ranged Attack': -7,
+    'Healing Word': 7
+}
+nemean_lion_hit_points = 68
+nemean_lion_attack_power = 25
+nemean_lion_attack_dict = {
+    'Bite': -9,
+    'Claws': -12
+}
+hydra_hit_points = 172
+hydra_attack_power = 25
+hydra_attack_dict = {
+    'Bite': -10,
+    'Tail Swipe': -16
+}
+cerberus_hit_points = 168
+cerberus_attack_power = 31
+cerberus_attack_dict ={
+    'Bite': -14,
+    'Fire Breath': -32,
+    'Scratch': -11
+}
