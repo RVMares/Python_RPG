75515ccf5da9b13c7058a0dfe30daa72b8ed7c6e
Ruth Hermosillo <hermosilloruth59@gmail.com>
2022-08-02 16:27:08 -0600

    Began Adding the Storyline and Defined the Function Start_RPG

--- main.py
+++ main.py
@@ -27,3 +27,17 @@ cerberus_attack_dict ={
     'Fire Breath': -32,
     'Scratch': -11
 }
+
+print('Welcome Great Hero!!!')
+print('You have been tasked by King Eurystheus to complete your penance!')
+print('You may be asking \"What penance, what did I do?\" I shall explain...')
+print('It is not entirely your fault for Hera was jealous and drove you to madness!')
+print('Alas, your beloved Megara was an innocent victim. You must avenge her and purify your sin!')
+def start_RPG():
+    game_start = input ('Are you up to the challenge? For you must slay the Nemean Lion! Defeat the Nine-Headed Hydra! And capture the guard dog of the underworld... Cerberus! Enter y/n: ')
+    if game_start == 'y':
+        print ('Make sure your weapons and mind are sharp! For the challenge begins!')
+    else:
+        print ('What is this? Are you defeated so quickly? Who can defeat the Mighty Hercules?')
+        start_RPG()
+start_RPG()
